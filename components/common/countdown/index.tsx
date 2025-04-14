a1bfd068c92481006132ef6bb8bff27803ef596b
Stephen Osemene Emmanuel <64918381+Emmanuelsteph7@users.noreply.github.com>
2025-04-14 11:47:23 +0100

    Update index.tsx

--- components/common/countdown/index.tsx
+++ components/common/countdown/index.tsx
@@ -6,9 +6,6 @@ import React, { useEffect, useState } from "react";
 import CountdownItem from "./countdownItem";
 import { calculateTimeLeft } from "@/utils/calculateTimeLeft";
 
-/**
- * 1st of May, 2025
- */
 const TARGET_DATE = "2025-05-15T00:00:00";
 
 export interface ITimeLeft {
